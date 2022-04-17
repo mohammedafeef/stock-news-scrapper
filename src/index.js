a95609d90d90496f0af82139c712ec7b6a44c867
mohammedafeef <mohammedafeef.maf@gmail.com>
2022-04-17 15:30:36 +0530

    feat: complete scrapper and add auto scroller to get all news

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const browserObject = require('./browser');
-const scraperController = require('../pageController');
+const scraperController = require('./pageController');
 
 //Start the browser and create a browser instance
 let browserInstance = browserObject.startBrowser();
--- src/pageScraper.js
+++ src/pageScraper.js
@@ -1,11 +1,31 @@
+const autoScroll = require("./autoScroller");
+
 const scraperObject = {
-	url: 'http://books.toscrape.com',
-	async scraper(browser){
-		let page = await browser.newPage();
-		console.log(`Navigating to ${this.url}...`);
-		await page.goto(this.url);
-		
-	}
-}
+  url: "https://economictimes.indiatimes.com/markets/stocks/news",
+  async scraper(browser) {
+    let page = await browser.newPage();
+    console.log(`Navigating to ${this.url}...`);
+    await page.goto(this.url);
+	
+    await page.setViewport({
+      width: 1200,
+      height: 800,
+    });
+    await autoScroll(page);
+
+    await page.waitForSelector(".tabdata");
+
+    let newsObjs = await page.$$eval("div.eachStory", (news) =>
+      news.map((newsDom) => ({
+        title: newsDom.querySelector("h3 > a").textContent,
+        content: newsDom.querySelector("p").textContent,
+        time: newsDom.querySelector("time").textContent,
+        imageUrl: newsDom.querySelector("span > span > img")?.src,
+      }))
+    );
+
+    console.log(newsObjs);
+  },
+};
 
 module.exports = scraperObject;
